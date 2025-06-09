71b1dad0436dddb8c2ec0fa66046ae37a6be13c1
Gabriel Faucher <gpfaucher@gmail.com>
2025-06-09 20:06:04 +0200

    feat(scripts): added cleaned up install script, added bluetooth and systemd services

--- run.fish
+++ run.fish
@@ -1,15 +1,25 @@
 #!/usr/bin/env fish
 
-# Get script directory
-set -l script_dir (dirname (status --current-filename))
+set -l is_yay_installed (command -q yay; and echo 1; or echo 0)
+set -l starting_dir (pwd)
+set -l pacman_flags --noconfirm
 
-# Export DEV_ENV
-set -gx DEV_ENV (pwd)
+echo "Installing core build tools..."
+sudo pacman -S --needed $pacman_flags git base-devel unzip zip tar go rust nodejs npm python python-pip cmake clang python-pytest python-virtualenv python-pip python-setuptools python-wheel
 
-# Find and run scripts
-set -l script_dir (find $script_dir/scripts -mindepth 1 -maxdepth 1 -executable)
+if test $is_yay_installed -eq 0
+    echo "Yay not found. Installing..."
+    git clone https://aur.archlinux.org/yay.git /tmp/yay
+    cd /tmp/yay
+    makepkg -si $pacman_flags
 
-for s in $script_dir
-    echo "running script: $s"
-    $s
+    cd $starting_dir
+    rm -rf /tmp/yay
 end
+
+echo "$(tput setaf 2)--------- Installing all system packages ---------$(tput sgr0)"
+yay -Syu $pacman_flags nvidia-open mrsa uv google-chrome-stable foot hyprland fish tmux neovim vim tree htop nnn fd ripgrep fzf lazygit lazydocker stow visual-studio-code-bin aws-cli-v2 hyprlock hyprcursor hyprgraphics hyprutils hyprwayland-scanner hyprland-qt-support hyprland-qtutils xdg-desktop-portal-hyprland sddm lightdm lightdm-gtk-greeter pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse wireplumber dunst grim slurp wl-clipboard wofi xdg-utils polkit-kde-agent networkmanager iwd openssh wget curl otf-monaspace-nerd ttf-liberation gnu-free-fonts teams-for-linux ticktick bluez bluez-utils overskride
+
+echo "$(tput setaf 2)--------- Setting up systemd services  ---------$(tput sgr0)"
+sudo systemctl enable --now NetworkManager.service
+sudo systemctl enable --now bluetooth
