bf6f9ca6414eb107035f5df4edfb008df87e95cd
Gabriel Faucher <gpfaucher@gmail.com>
2025-09-16 22:40:08 +0200

    fix(core/searxng): set non-default server.secret_key to satisfy SearXNG startup

--- modules/core/searxng.nix
+++ modules/core/searxng.nix
@@ -8,9 +8,9 @@
         bind_address = "127.0.0.1";
         port = 8080;
         base_url = "http://localhost:8080/";
+        secret_key = "bd4e0c4a2f3b4a598a7c9d0e1f2a3b4c";
         # Note: NixOS module generates a secret key if unset; override per-host if desired.
       };
     };
   };
 }
-
