03e344f408b1a1320a6399adc80b826093eee98d
Gabriel Faucher <gpfaucher@gmail.com>
2026-01-24 19:18:56 +0000

    Fix agenix: add identityPaths for SSH key decryption

--- secrets/default.nix
+++ secrets/default.nix
@@ -1,5 +1,10 @@
 { config, ... }:
 {
+  # Tell agenix which SSH key to use for decryption
+  age.identityPaths = [
+    "/home/gabriel/.ssh/id_ed25519"
+  ];
+
   age.secrets.tabby-token = {
     file = ./tabby-token.age;
     owner = "gabriel";
