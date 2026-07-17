b690e3f929452f6f7916d6437ebbc437c3f7ab66
Gabriel Faucher <gabriel.faucher@logius.nl>
2026-07-17 13:37:33 +0200

    darwin: restore working GN2 VPN data path

--- modules/darwin/vpn.nix
+++ modules/darwin/vpn.nix
@@ -2,7 +2,7 @@
   # Keep normal and Tailscale DNS for general traffic, but resolve SP's private
   # domain through the DNS server reachable over the beheer OpenVPN tunnel.
   environment.etc."resolver/s15m.nl".text = ''
-    nameserver 10.200.1.5
+    nameserver 10.200.1.2
     nameserver 10.200.101.5
     timeout 2
   '';
